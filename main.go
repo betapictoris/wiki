d0e0476af34113776e7c11b45c2088aaffd353bf
beta <beta@ozx.me>
2022-09-21 17:29:31 -0700

    :lock: Lower (save) file perms from 666 to 400.

--- main.go
+++ main.go
@@ -172,7 +172,7 @@ func main() {
 	}
 
 	if saveToFile {
-		f, err := os.OpenFile(article+".md", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		f, err := os.OpenFile(article+".md", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0400)
 		if err != nil {
 			f.Close()
 			log.Fatal(err)
